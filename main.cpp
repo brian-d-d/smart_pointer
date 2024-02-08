2a4a6628164df6b45737a3620e9557b547a2635c
briand <brian.dortpinar@zenobe.com>
2024-02-08 16:03:59 +1100

    unique ptr and weak ptr tests

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "myshared_ptr_gmock.h"
 #include "unique_ptr_gtest.h"
 #include <gtest/gtest.h>
+#include <iostream>
 
 using ::testing::AtLeast;
 using ::testing::Return;
@@ -29,9 +30,55 @@ using ::testing::Return;
 
 // }
 
-TEST_F(unique_ptr_gtest<int>, ptr_value) {
+TEST_F(unique_ptr_gtest, ptr_value) {
     EXPECT_EQ(*(unique_ptr1.get()), 1);
+    EXPECT_EQ(*(unique_ptr2.get()), 2);
+    EXPECT_EQ(*(unique_ptr3.get()), 3);
 }
+
+//Checks if the unique pointer releases the ownership of the pointer
+TEST_F(unique_ptr_gtest, release) {
+    unique_ptr1.release();
+    EXPECT_EQ(unique_ptr1.get(), nullptr);
+}
+
+//Checks if ownership of the pointer is transferred between unique pointers
+TEST(unique_ptr_gtest_no_class, ownership1) {
+    std::unique_ptr<int> unique_ptr1(new int (1));
+    std::unique_ptr<int> unique_ptr2(std::move(unique_ptr1));
+
+    EXPECT_EQ(unique_ptr1.get(), nullptr);
+    EXPECT_EQ(*(unique_ptr2.get()), 1);
+}
+
+//Checks that the ownership of the num pointer is transferred to unique_ptr1 even after num goes out of scope
+TEST(unique_ptr_gtest_no_class, ownership2) {
+    std::unique_ptr<int> unique_ptr1;
+    if (true) {
+        int *num = new int(5);
+        unique_ptr1 = std::unique_ptr<int>(std::move(num));
+    }
+
+    EXPECT_EQ(*(unique_ptr1.get()), 5);
+}
+
+//Checks that the weak pointer does not stop num from being deleted after shared_ptr1 goes out of scope
+TEST(weak_ptr_gtest_no_class, ownership1) {
+    std::weak_ptr<int> weak_ptr1;
+    if (true) {
+        int *num = new int(5);
+        std::shared_ptr<int> shared_ptr1(std::move(num));
+        weak_ptr1 = shared_ptr1;
+
+        EXPECT_EQ(*((weak_ptr1.lock()).get()), 5);
+    }
+
+    EXPECT_EQ(weak_ptr1.expired(), true);
+}
+
+
+
+
 
 
 
--- unique_ptr_gtest.h
+++ unique_ptr_gtest.h
@@ -4,18 +4,18 @@
 #include <memory>
 #include <gtest/gtest.h>
 
-template <class T>
+// template <class T>
 class unique_ptr_gtest : public testing::Test {
     protected:
         void SetUp() override {
-            unique_ptr1 = std::unique_ptr<T>(new T(1));
-            unique_ptr2 = std::unique_ptr<T>(new T(2));
-            unique_ptr3 = std::unique_ptr<T>(new T(3));
+            unique_ptr1 = std::unique_ptr<int>(new int(1));
+            unique_ptr2 = std::unique_ptr<int>(new int(2));
+            unique_ptr3 = std::unique_ptr<int>(new int(3));
         }
 
-        std::unique_ptr<T> unique_ptr1;
-        std::unique_ptr<T> unique_ptr2;
-        std::unique_ptr<T> unique_ptr3;
+        std::unique_ptr<int> unique_ptr1;
+        std::unique_ptr<int> unique_ptr2;
+        std::unique_ptr<int> unique_ptr3;
 };
 
 #endif
